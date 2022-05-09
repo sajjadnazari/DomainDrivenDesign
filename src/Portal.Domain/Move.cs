fae06789f5ef04c581706c66413128d0c7362c04
Sajjad Nazari <sajjad69nazari@gmail.com>
2022-05-09 12:36:31 +0430

    Add Domain and Xunit

---
+++ src/Portal.Domain/Move.cs
@@ -0,0 +1,8 @@
+﻿namespace Portal.Domain
+{
+    public class Move
+    {
+        public Player Player { get; set; }
+        public Position Position { get; set; }
+    }
+}
---
+++ src/Portal.Domain/Position.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Domain
+{
+    public enum PositionState
+    {
+        None = 0,
+        X = 1,
+        O = 2
+    }
+    public enum PositionType
+    {
+        None = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9
+    }
+    public class Position
+    {
+        public PositionState State { get; set; }
+        public PositionType Type { get; set; }
+    }
+}
